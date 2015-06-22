2d1fd5fe7d208bb7ede14341ab8effbf3cb81e18
Joseph Buchma <josephbuchma@gmail.com>
2015-06-22 19:43:17 +0300

    a lot of files

--- make_one_thousand_tests.rb
+++ make_one_thousand_tests.rb
@@ -1,12 +1,12 @@
 
 tests = ''
-1000.times do |i|
+4.times do |i|
   tests += "it 'test something #{i.to_s}' do\n  expect(true).to be_falsey\nend \n"
 end
 
 buffer = "require 'rspec'\n\ndescribe 'one thousand tests' do\n #{tests} \nend\n"
 
-10.times do |i|
+500.times do |i|
   f = File.new("spec/one_thousand_tests#{i.to_s}_spec.rb", 'w')
   f.write(buffer)
   f.close()
